587b74cca8913ae0bd5e76b2315af8a93d30bccc
WillKre <7396157+willkre@users.noreply.github.com>
2022-11-26 00:24:48 +0000

    style: add & configure vanilla-extract

---
+++ src/App.css.ts
@@ -0,0 +1,7 @@
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+	background: "#222",
+	minHeight: "100vh",
+	minWidth: "100vw",
+});
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { container } from "./Header.css";
+
+export function Header() {
+	return (
+		<div className={container}>
+			<h1>Mintz</h1>
+		</div>
+	);
+}
